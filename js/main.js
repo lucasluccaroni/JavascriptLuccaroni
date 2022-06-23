d43fc35e2239ea0f3b97ea0b404da488cf218e19
lucas.luccaroni <lucas_luccaroni@hotmail.com.ar>
2022-06-22 22:56:52 -0300

    entrega 22.06

--- js/main.js
+++ js/main.js
@@ -12,23 +12,25 @@ while ((nombreUsuario == null) || (nombreUsuario == "") || (nombreUsuario == " "
 
 alert("Bienvenido " + nombreUsuario + "!");
 
+
 let pelicula;
-let pelicula1 = "1- Star Wars Capítulo 4.";
-let pelicula2 = "2- Harry Potter y el Prisionero de Askaban.";
-let pelicula3 = "3- El Señor de los Anillos: Las Dos Torres.";
 
 function elegir_pelicula () {
     pelicula = prompt("Elija que pelicula desea ver: \n 1. Star Wars Capítulo 4. \n 2. Harry Potter y el Prisionero de Askaban. \n 3. El Señor de los Anillos: Las Dos Torres");
     if (pelicula === "1"){
-        alert("Usted eligió 'Star Wars Capítulo 4'. La duracion de la pelicula es de 121 minutos. \n Que la disfrutes!");
+        alert("Usted eligió 'Star Wars Capítulo 4'. La duracion de la pelicula es de 121 minutos. \n ¡Que la disfrutes!");
+
     } else if (pelicula === "2"){
         alert("Usted eligió 'Harry Potter y el Prisionero de Askaban'. La duracion de la pelicula es de 137 minutos. \n ¡Que la disfrutes!");
+
     } else if (pelicula === "3"){
-        alert("Usted eligió 'El Señor de los Anillos: Las Dos Torres'. La duracion de la pelicula es de 179 minutos. \n Que la disfrutes!");
+        alert("Usted eligió 'El Señor de los Anillos: Las Dos Torres'. La duracion de la pelicula es de 179 minutos. \n ¡Que la disfrutes!");
+        
     } else {
-        alert ("Por favor escriba 1, 2 o 3 para elegir la película deseada. ");
-        //no puedo buclear la funcion para que me ofrezca nuevamente el catálogo.
+        alert ("Por favor escriba 1, 2 o 3 para elegir la película deseada.");
+        elegir_pelicula();
     }
-}    
-elegir_pelicula();
+}
+ elegir_pelicula();
+
 
